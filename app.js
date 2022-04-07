f430a53a3c5ed0ceed255af66526a5bdeda1cdf2
Ch Sahithi <chsahithi.3812@gmail.com>
2022-04-07 20:53:00 +0530

    backend

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const app=express()
 const cors= require('cors')
 require('dotenv').config()
 
-require('./database/db.js')
-
+require('./src/database/db.js')
 
 app.use(express.json())
 app.use(cors())
 
-
-
-app.use(require('./routes/auth.js'))
+app.use(require('./src/routes/auth.js'))
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)

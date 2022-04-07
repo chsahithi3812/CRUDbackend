fe697e9ede0cd6331d1c020d3784c35d50dcbef0
Ch Sahithi <chsahithi.3812@gmail.com>
2022-04-07 19:52:26 +0530

    backend

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require ('express')
 const port=process.env.PORT || 9000
 const app=express()
 const cors= require('cors')
+require('dotenv').config()
 
 require('./database/db.js')
 
